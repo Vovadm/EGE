fc52aa4152549fc7c34e976f72c61e2840244da9
raild <vovadm@hotmail.com>
2025-12-11 15:07:08 +0300

    15 fix

--- dec/task15.4-08.12/1.py
+++ dec/task15.4-08.12/1.py
@@ -1,9 +1,9 @@
-Q = list(range(17, 91))
-P = list(range(10, 47))
+Q = list(range(17, 91 + 1))
+P = list(range(10, 47 + 1))
 A = []
 for x in range(1, 100):
     if not (((x in P) and (x in Q)) <= (x in A)):
         A.append(x)  # наименьшая длина
 
 print(A)
-print(len(A))
+print(print(A[-1] - A[0]))
