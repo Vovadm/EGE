60ff3562de203381bd5537c4e9a6001505b47aed
raild <vovadm@hotmail.com>
2026-01-11 23:32:39 +0300

    task19-21

--- jan/task19:21.3-09.01/6.py
+++ jan/task19:21.3-09.01/6.py
@@ -1,14 +1,14 @@
 def g(s, p, end):
-    if s >= 91 and p == end:
+    if s >= 91 and p in end:
         return 1
 
-    if s < 91 and p == end:
+    if s < 91 and p == max(end):
         return 0
 
-    if s >= 91 and p != end:
+    if s >= 91 and p not in end:
         return 0
 
-    if (p + 1) % 2 == (end % 2):
+    if (p + 1) % 2 == (end[0] % 2):
         return (
             g(s + 1, p + 1, end)
             or g(s + 4, p + 1, end)
@@ -24,5 +24,5 @@ def g(s, p, end):
 
 
 for s in range(1, 91):
-    if g(s, 0, 3):
+    if g(s, 0, [2, 4]) and not (g(s, 0, [2])):
         print(s)
