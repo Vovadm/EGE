0dea1ab8dd8257a815f970310325bafa0645c2bf
Raild <86412430+Vovadm@users.noreply.github.com>
2026-02-03 23:49:56 +0300

    examinf

---
+++ other/examinf/16/461.py
@@ -0,0 +1,16 @@
+from functools import lru_cache
+import sys
+
+sys.setrecursionlimit(5000)
+
+
+@lru_cache(None)
+def F(n):
+    if n > 2024:
+        return 2
+    if n == 2024:
+        return 1
+    return n * (n + 1) + F(n + 1) - F(n + 2)
+
+
+print(F(100) - F(10) + F(2020))
