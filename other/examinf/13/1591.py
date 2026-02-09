7742cf6af02f5fe5bb3bd28767668b67ef3fdc82
Raild <86412430+Vovadm@users.noreply.github.com>
2026-02-09 20:22:49 +0300

    examinf

--- other/examinf/13/1591.py
+++ other/examinf/13/1591.py
@@ -5,4 +5,4 @@ net = ipaddress.ip_network("167.66.136.176/255.254.0.0", strict=False)
 ls = list(net.hosts())
 min_ip = sorted(ls)[-1]
 print(min_ip)
-print(167+67+255+254)
+print(167 + 67 + 255 + 254)
